0df1576972678ee1f92be4c10c5d6ef87666a41f
Joao-Inacio <joaoinacio206@protonmail.com>
2024-07-23 13:01:57 -0300

    Aula 1.2 - Aprenda a DEBUGAR em Linguagem C

---
+++ Curso-de-Linguagem-C/Aula-1.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("mensagem 1 ");
+    printf("mensagem 2 ");
+    printf("mensagem 3 ");
+    printf("mensagem 4 ");
+    printf("mensagem 5 ");
+    printf("mensagem 6 ");
+    return 0;
+}
+
