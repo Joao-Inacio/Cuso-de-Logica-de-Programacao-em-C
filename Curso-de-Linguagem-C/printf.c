2f6ee28cabaeb48c464b3d8a9dfe77e26e9045da
Joao-Inacio <joaoinacio206@protonmail.com>
2024-07-29 15:48:30 -0300

    Usando printf para exibir uma mensagem

---
+++ Curso-de-Linguagem-C/printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+	
+	printf("Oi, tudo bem? Tenho 20 e programo.\n");
+	
+	printf("Valor inteiro: %d. \n", 10);
+	printf("Valor Real: %f. \n", 3.14159265);
+	printf("Valor Real com apenas duas casas %.2f. \n", 3.14159265);
+	printf("Valor de texto: %c. \n", 'a');
+	printf("Valor de texto: %s. \n", "testando");
+}
