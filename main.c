e7286f095605d7c63484b46bd10d6adfadddcbc5
Joao-Inacio <joaoinacio206@protonmail.com>
2024-07-31 14:17:28 -0300

    Aula de C do curso de automação com IoT

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World! \n");
+    printf("João Inácio \n");
+    printf(
+    	"Hello, World! \n"
+    	"João Inácio \n"
+    	"Introdução á Linguagem C\n"
+	);
+    getch(); 
+}
