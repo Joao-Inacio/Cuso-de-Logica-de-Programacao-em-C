951e2ae0a85d8269e0c779e903fda143924a5c13
Joao-Inacio <joaoinacio206@protonmail.com>
2024-08-22 10:19:46 -0300

    Atividade avaliativa 2 questão 1

---
+++ Modulo-Logica-de-Programacao-em-C/Atividade2-Q1.c
@@ -0,0 +1,11 @@
+// 1. Faça um programa que mostre apenas os números pares dentro do intervalo de 0 até 20. 
+// Use o laço while para solucionar esse problema.
+#include <stdio.h>
+
+
+int main(){
+    int i;
+    for (i = 2; i <= 20; i+=2){
+        printf("%d\n", i);
+    }
+}
