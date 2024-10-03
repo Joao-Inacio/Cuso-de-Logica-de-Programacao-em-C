8acda6e05ed50a5b8b93562d61171fea5a7d068f
Joao-Inacio <joaoinacio206@protonmail.com>
2024-10-03 10:19:04 -0300

    Atividade pratica de logica de programação em C questão 1 da aula 5

---
+++ Modulo-Logica-de-Programacao-em-C/Aula-5/Atv-5.1.c
@@ -0,0 +1,9 @@
+// 1. Elabore um programa para ler um número digitado e mostrar o seu valor na tela.
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+    printf("\nO numero digitado foi %d", numero);
+}
